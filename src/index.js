8479ba63e7366794abfcec581616002a74fe3ed6
Anthony <anthony.kizer88@gmail.com>
2019-09-04 15:48:39 -0700

    finished mvp

--- src/index.js
+++ src/index.js
@@ -9,15 +9,19 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=${page}&sparkline=true`
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {
+        console.log(res);
+        setCoinData(res.data);
+      })
       .catch(err => console.log(err));
-  }, []);
+  }, [page]);
   return (
     <div className="App">
       <Navbar />
